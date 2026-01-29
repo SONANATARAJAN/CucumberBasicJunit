b88cd577811aba596e91fcd2a627409760f3aca2
Sona N <110325932+SONANATARAJAN@users.noreply.github.com>
2026-01-30 00:04:57 +0530

    Update login_pom.java

--- src/test/java/tests/login_pom.java
+++ src/test/java/tests/login_pom.java
@@ -4,44 +4,40 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.LoginPage;
-
-import java.time.Duration;
+import stepsforHooks.Hook;
 
 public class login_pom {
-    public WebDriver driver ;
+
+    WebDriver driver;
+
+    public login_pom() {
+        // ✅ Reuse driver from Hook
+        this.driver = Hook.driver;
+    }
 
     @Given("user is on login page")
-    public void userIsOnLoginPage() throws InterruptedException {
-        driver=new ChromeDriver();
+    public void userIsOnLoginPage() {
         driver.get("https://practicetestautomation.com/practice-test-login/");
-        driver.manage().window().maximize();
-        Thread.sleep(4000);
-     }
+    }
+
     @When("^user enters (.*) and (.*)$")
-    public void userEntersUsernameAndPassword(String username , String password) throws InterruptedException {
-        LoginPage loginPage=new LoginPage(driver);
+    public void userEntersUsernameAndPassword(String username, String password) {
+        LoginPage loginPage = new LoginPage(driver);
         loginPage.enterUsername(username);
         loginPage.enterPassword(password);
-
     }
+
     @And("click on logic button")
-    public void clickOnLogicButton() throws InterruptedException {
-        LoginPage loginPage=new LoginPage(driver);
+    public void clickOnLogicButton() {
+        LoginPage loginPage = new LoginPage(driver);
         loginPage.click();
     }
 
-
     @Then("user is navigated to home page")
     public void userIsNavigatedToHomePage() {
-        LoginPage loginPage=new LoginPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
         loginPage.flg_afterLoginCheck();
-     }
-
+    }
 }

c0c24652d03c7e9b877e3d8fde9f21390e1fe7b3
Sona N <110325932+SONANATARAJAN@users.noreply.github.com>
2026-01-29 22:46:29 +0530

    Update Hook.java

--- src/test/java/stepsforHooks/Hook.java
+++ src/test/java/stepsforHooks/Hook.java
@@ -4,6 +4,7 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,18 +14,18 @@ public class Hook {
     @Before
     public void browserSetup(){
         ChromeOptions options = new ChromeOptions();
-options.addArguments("--headless");
-options.addArguments("--no-sandbox");
-options.addArguments("--disable-dev-shm-usage");
-options.addArguments("--window-size=1920,1080");
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
 
- 
+
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-       // driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        // driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
     }
     @After
     public void browserShutDown(){
-  driver.close();
+        driver.close();
     }
 }
